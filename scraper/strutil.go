f040e9a669e702d84d2e023caebe62100d919f4d
Rosco Pecoltran <rosco_pecoltran@msn.com>
2017-11-10 17:32:12 +0100

    add reverse string method

--- scraper/strutil.go
+++ scraper/strutil.go
@@ -6,6 +6,15 @@ import (
 	"unicode/utf8"
 )
 
+// Reverse returns its argument string reversed rune-wise left to right.
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
+
 // abbrev abbreviates string s to a length of max.
 func abbrev(s string, max int) string {
 	if len(s) <= max {
